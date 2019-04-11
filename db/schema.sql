66b65979784c38bd289d748c4842a82c0ad98cc9
Matthew Trabold <mct4240@gmail.com>
2019-04-11 16:53:24 -0500

    Setting up the model

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS burger_db;
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
+CREATE TABLE burgers(
+	id INT AUTO_INCREMENT,
+	burger_name VARCHAR(50) NOT NULL,
+	devoured BOOLEAN DEFAULT FALSE,
+	PRIMARY KEY(id)
+);
