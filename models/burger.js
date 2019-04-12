0d3029542af0e33847e8fd22a192ec01e9f03184
Matthew Trabold <mct4240@gmail.com>
2019-04-12 18:36:37 -0500

    Burgers Debugged and Running

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,28 @@
-var connection = require("../config/orm.js");
+var orm = require("../config/orm.js");
+
+var burgers = {
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  // delete: function(condition, cb) {
+  //   orm.delete("burgers", condition, function(res) {
+  //     cb(res);
+  //   });
+  // }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burgers;
