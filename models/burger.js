f48c4382f526a47277c70323cfa5b4518180b375
Matthew Trabold <mct4240@gmail.com>
2019-04-12 19:37:58 -0500

    Push

--- models/burger.js
+++ models/burger.js
@@ -17,11 +17,11 @@ var burgers = {
       cb(res);
     });
   },
-  // delete: function(condition, cb) {
-  //   orm.delete("burgers", condition, function(res) {
-  //     cb(res);
-  //   });
-  // }
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
 };
 
 // Export the database functions for the controller (catsController.js).
